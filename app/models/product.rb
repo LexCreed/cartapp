f80ec0bbef447f5f5589f4eed7ac866cb954bd0a
Elias Abraham Hernandez Ramirez <naster0007@gmail.com>
2016-06-03 15:43:06 -0500

    cartapp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-has_many :hasBuyOrders
-has_many :buyOrders, through: :hasBuyOrders
-has_attached_file :cover, styles: {medium: "800x600", thumb: "80x80" }
-	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+
+	has_many :hasBuyOrders
+	has_many :buyOrders, through: :hasBuyOrders
+
+ 	has_attached_file :cover, styles: {thumb:"400x300", mini:"400x200"}
+    validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 
 end
