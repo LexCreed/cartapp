fa1c1b35c4383d0c8c2604d2450b4401c3da7b71
Elias Abraham Hernandez Ramirez <naster0007@gmail.com>
2016-06-07 12:38:54 -0500

    cartapp setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,6 @@ class Product < ActiveRecord::Base
 
  	has_attached_file :cover, styles: {thumb:"400x300", mini:"400x200"}
     validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+ 
 
 end
